df1b5ee16dc239ff5ea0926bdd5e8db6e1ccf793
Rozenberg-jv <avangard.npaper@gmail.com>
2021-09-22 04:39:55 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstapp';
+  title = 'Firstapp';
+  value = '12341151412312412';
+
+  constructor() {
+    setTimeout(() => {
+      this.value = 'asdbaegfbawefab';
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule
