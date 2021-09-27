f33135e5d255823c988a0d7060c9f4a51e4d16a6
Rozenberg-jv <avangard.npaper@gmail.com>
2021-09-27 05:18:27 +0300

    step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {MainpageComponent} from './mainpage/mainpage.component';
-import {SidemenuComponent} from './sidemenu/sidemenu.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainpageComponent,
-    SidemenuComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, MainpageComponent, SidemenuComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'mainpage',
@@ -7,17 +14,61 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./mainpage.component.css'],
 })
 export class MainpageComponent {
-  exampleForm = new FormGroup({
-    input1: new FormControl(''),
-    input2: new FormControl(''),
-  });
+  exampleForm;
+
+  constructor(private http: HttpClient) {
+    this.exampleForm = new FormGroup({
+      input1: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        this.aContValidator,
+      ]),
+      input2: new FormControl(''),
+    });
+  }
+
+  data = {
+    value1: '',
+    value2: '',
+  };
+
+  aContValidator(control: AbstractControl): { [s: string]: boolean } | null {
+    if (control.value && !control.value.includes('A')) {
+      return { acont: true };
+    }
+
+    return null;
+  }
+
+  getErrorList(list: any): string[] | null {
+    console.log(list);
+
+    return Object.keys(list ? list : {});
+  }
 
   submitForm() {
-    console.log(this.exampleForm);
+    // console.log(this.exampleForm);
 
     console.log(
       this.exampleForm.controls.input1.value,
       this.exampleForm.controls.input2.value
     );
+
+    this.data.value1 = this.exampleForm.controls.input1.value.trim();
+    this.data.value2 = this.exampleForm.controls.input2.value.trim();
+
+    this.exampleForm.reset();
+  }
+
+  /* http */
+
+  download() {
+    console.log('download');
+
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .subscribe((response) => {
+        console.log('response', response);
+      });
   }
 }
